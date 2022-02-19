421335db1a91cfe1ab1438ac9b533556b6f57f47
hinga- <higa0208@gmail.com>
2022-02-19 20:01:02 +0900

    :tada:

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import 'styles/index.scss';
 
-import App from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GridNoise } from 'sketch/GridNoise';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GridNoise />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/sketch/GridNoise/GridNoise.tsx
@@ -0,0 +1,76 @@
+import { makeNoise3D } from 'fast-simplex-noise';
+import { useControls } from 'leva';
+import { P5Instance, ReactP5Wrapper } from 'react-p5-wrapper';
+
+const noise3D = makeNoise3D(Math.random);
+
+const sketch = (p5: P5Instance) => {
+  const props = { count: 0, size: 0, amplitude: 0, frequency: 0 };
+
+  p5.updateWithProps = ({ count, size, amplitude, frequency }) => {
+    props.count = count;
+    props.size = size;
+    props.amplitude = amplitude;
+    props.frequency = frequency;
+  };
+
+  p5.setup = () => {
+    // p5.frameRate(30);
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+  };
+
+  p5.draw = () => {
+    p5.background(255);
+    p5.push();
+    p5.noStroke();
+    p5.translate(
+      p5.windowWidth / props.count / 2,
+      p5.windowHeight / props.count / 2
+    );
+
+    const width = p5.windowWidth;
+    const height = p5.windowHeight;
+    const time = p5.millis();
+
+    for (let x = 0; x < props.count; x++) {
+      for (let y = 0; y < props.count; y++) {
+        const px = (width / props.count) * x;
+        const py = (height / props.count) * y;
+        const n =
+          props.amplitude *
+          noise3D(
+            props.frequency * (x + time * 0.002),
+            props.frequency * (y + time * 0.002),
+            props.frequency * (x / y + time * 0.002)
+          );
+        const size = p5.map(n, -1, 1, 0, props.size);
+        p5.ellipse(px, py, size);
+        p5.fill(0);
+      }
+    }
+
+    p5.pop();
+  };
+
+  p5.windowResized = () => p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+};
+
+const GridNoise = () => {
+  const { count, size, amplitude, frequency } = useControls({
+    count: { value: 40, min: 1, max: 100, step: 1 },
+    size: { value: 3, min: 0.1, max: 10, step: 0.1 },
+    amplitude: { value: 1, min: 0, max: 1, step: 0.01 },
+    frequency: { value: 0.08, min: 0, max: 1, step: 0.001 },
+  });
+
+  return (
+    <div className="canvas">
+      <ReactP5Wrapper
+        sketch={sketch}
+        {...{ count, size, amplitude, frequency }}
+      />
+    </div>
+  );
+};
+
+export default GridNoise;
